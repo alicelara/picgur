72808387d074e3b2d0ba7317991d3ee78e5bf51c
Alice Lara <hello@alicelara.ca>
2013-07-07 17:23:33 -0400

    show page working

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,32 +21,27 @@ class PicturesController < ActionController::Base
 		]
 	end
 
-	def picture0
-				@picture = 
+	def show
+				@pictures = [
 			{
 				:title => 'Penguin',
 				:artist => 'pingu',
 				:url => 'http://upload.wikimedia.org/wikipedia/en/c/c2/Pingu.png'
-				}
-	end
+				},
 
-	def picture1
-				@picture =
-				{
+			{
 				:title => 'Baby Panda',
 				:artist => 'animaldoor',
 				:url => 'http://2.bp.blogspot.com/-5IAKaZek9eQ/TgA5eWTCb-I/AAAAAAAADng/aOMu3B_8Fuw/s400/baby_panda_by_PLuSSbjk.jpg'
-				}
-	end
-
-	def picture2
-		@picture = 
+				},	
 			{
 				:title => 'Sloth',
 				:artist => 'mysterious universe',
 				:url => 'http://mysteriousuniverse.org/wp-content/uploads/2013/03/Sloth-570x427.jpg'
 				}	
-		
+		]
+			@picture = @pictures[params[:id].to_i]
 	end
+
 
 end
