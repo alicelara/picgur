4baca70387bdf6d765207ac4dd7e3a56f55b4217
Alice Lara <hello@alicelara.ca>
2013-07-07 19:20:56 -0400

    Populating the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+	:title => 'Penguin',
+	:artist => 'pingu',
+	:url => 'http://upload.wikimedia.org/wikipedia/en/c/c2/Pingu.png'
+	)
+
+Picture.create!(
+	:title => 'Baby Panda',
+	:artist => 'animaldoor',
+	:url => 'http://2.bp.blogspot.com/-5IAKaZek9eQ/TgA5eWTCb-I/AAAAAAAADng/aOMu3B_8Fuw/s400/baby_panda_by_PLuSSbjk.jpg'
+)
+
+Picture.create!(
+	:title => 'Sloth',
+	:artist => 'mysterious universe',
+	:url => 'http://mysteriousuniverse.org/wp-content/uploads/2013/03/Sloth-570x427.jpg'
+)
