083f0fc0505034ca21b3f4b65b96fe0fb8d5a93c
Alice Lara <hello@alicelara.ca>
2013-07-07 17:33:18 -0400

    added new get and create post methods in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Picgur::Application.routes.draw do
-
+  
+  post 'picture' => 'pictures#create'
   get 'pictures' => 'pictures#index'
+  get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
 
 end
