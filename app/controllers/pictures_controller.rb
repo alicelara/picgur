96b35e9ab3422e97554102206a5e7837cca4ed88
Alice Lara <hello@alicelara.ca>
2013-07-07 17:02:10 -0400

    defined pic1 and pic2  methods and filled their html pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -17,7 +17,7 @@ class PicturesController < ActionController::Base
 				:title => 'Sloth',
 				:artist => 'mysterious universe',
 				:url => 'http://mysteriousuniverse.org/wp-content/uploads/2013/03/Sloth-570x427.jpg'
-				},	
+				}	
 		]
 	end
 
@@ -30,5 +30,23 @@ class PicturesController < ActionController::Base
 				}
 	end
 
+	def picture1
+				@picture =
+				{
+				:title => 'Baby Panda',
+				:artist => 'animaldoor',
+				:url => 'http://2.bp.blogspot.com/-5IAKaZek9eQ/TgA5eWTCb-I/AAAAAAAADng/aOMu3B_8Fuw/s400/baby_panda_by_PLuSSbjk.jpg'
+				}
+	end
+
+	def picture2
+		@picture = 
+			{
+				:title => 'Sloth',
+				:artist => 'mysterious universe',
+				:url => 'http://mysteriousuniverse.org/wp-content/uploads/2013/03/Sloth-570x427.jpg'
+				}	
+		
+	end
 
 end
