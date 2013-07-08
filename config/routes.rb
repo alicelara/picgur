a59729f6f968751e00f7f066bc7d50cc49676fae
Alice Lara <hello@alicelara.ca>
2013-07-07 20:32:06 -0400

    add links to pages and create edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Picgur::Application.routes.draw do
   root :to => 'pictures#index'
-  
+
   post 'pictures' => 'pictures#create'
   get 'pictures' => 'pictures#index'
   get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
+  get 'pictures/:id/edit' => 'pictures#edit'
 
 
 end
