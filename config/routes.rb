de1e55e292e1503ae57f08519c96b952f272ad39
Alice Lara <hello@alicelara.ca>
2013-07-07 20:20:47 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Picgur::Application.routes.draw do
+  root :to => 'pictures#index'
   
   post 'pictures' => 'pictures#create'
   get 'pictures' => 'pictures#index'
   get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
+
 
 end
